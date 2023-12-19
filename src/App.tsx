631bd63c2bd4390628a78051d2306c4be46cebc8
DESKTOP-MC950J7\This PC <lephuocthanhcao@gmail.com>
2023-12-19 18:57:52 +0700

    using iframe to embed code preview screen into the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import React, {useEffect, useRef, useState} from 'react';
-// import './App.css';
 import * as esbuild from 'esbuild-wasm';
 import {unpkgPathPlugin} from './plugins/unpkg-path-plugin';
 import {fetchPlugin} from './plugins/fetch-plugin';
 
 const App: React.FC = () => {
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-  const esbuildRef = useRef(false);
+  const esbuildRef = useRef<any>(false);
+  const iframeRef = useRef<any>();
 
   useEffect(() => {
     startService();
-    esbuildRef.current = true;
   }, []);
 
   const startService = async () => {
     if (esbuildRef.current) return; 
     await esbuild.initialize({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.19.9/esbuild.wasm'
     });
+    esbuildRef.current = true;
   };
 
   const onClick = async () => {
-    if (!esbuildRef.current) return;
+    if (!esbuildRef.current || !iframeRef?.current) return;
+
+    iframeRef.current.srcdoc = html;
 
     const result = await esbuild.build({
       entryPoints: ['index.js'],
@@ -38,16 +39,37 @@ const App: React.FC = () => {
         global: 'window'
       }
     })
-    setCode(result.outputFiles[0].text);
+
+    iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.getElementById('root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }  
+        }, false);
+      </script>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
-      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <textarea rows={5} cols={39} value={input} onChange={e => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
         </div>
-      <pre>{code}</pre>
+      <iframe title='preview' ref={iframeRef} srcDoc={html} sandbox='allow-scripts'/>
     </div>
   );
 }
